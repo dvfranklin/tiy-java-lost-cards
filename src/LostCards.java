156b6db8ee2c3b13985ca0dd8ec52e0ec42622a0
Dan <dvfranklin@gmail.com>
2016-04-14 17:17:34 -0400

    Completed Lost Cards, passing tests

--- src/LostCards.java
+++ src/LostCards.java
@@ -57,5 +57,128 @@
  *
  */
 public class LostCards {
+    public static void main(String[] args){
 
+        // counter for total number of cards
+        int countCards = 0;
+
+        // loop through suits
+        for(int suit=1; suit <= 4; suit++){
+
+            // write out suits to check for length
+            String suitText = "";
+            switch(suit){
+                case 1:
+                    suitText = "Clubs";
+                    break;
+                case 2:
+                    suitText = "Diamonds";
+                    break;
+                case 3:
+                    suitText = "Hearts";
+                    break;
+                case 4:
+                    suitText = "Spades";
+                    break;
+            }
+
+            // loop through numbers
+            for(int num=1; num <= 13; num++){
+
+                String card = "";
+                String suitSymbol = "";
+                boolean isMissing = false;
+                boolean isTorn = false;
+                boolean isDamaged = false;
+                boolean isDown = false;
+
+                if ((num == 13 && suit == 4) || (num == 3 && suit != 2)){
+                    isMissing = true;
+                }
+
+                if ((num % 3 == 0)  && (suit == 2 || suit == 3)){
+                    isTorn = true;
+                }
+
+                if ((num == 1 || num == 11 || num == 12 || num == 13) && (suit == 1 || suit == 3)){
+                    isDamaged = true;
+                }
+
+                if (num == suitText.length()){
+                    isDown = true;
+                }
+
+                // change suitText to use suit symbols
+                switch(suit){
+                    case 1:
+                        suitSymbol = "♣";
+                        break;
+                    case 2:
+                        suitSymbol = "♢";
+                        break;
+                    case 3:
+                        suitSymbol = "♡";
+                        break;
+                    case 4:
+                        suitSymbol = "♠";
+                        break;
+                }
+
+                switch(num){
+                    case 1:
+                        card = "A";
+                        break;
+                    case 10:
+                        card = "t";
+                        break;
+                    case 11:
+                        card = "J";
+                        break;
+                    case 12:
+                        card = "Q";
+                        break;
+                    case 13:
+                        card = "K";
+                        break;
+                    default:
+                        Integer cardInt = new Integer(num);
+                        card = cardInt.toString();
+                }
+
+                // if card is in the deck, print it out
+                if(!isMissing){
+
+                    if(isTorn && isDamaged && isDown){
+                        System.out.println("-~**~-");
+                        countCards++;
+                    } else if(isTorn && isDown){
+                        System.out.println("-**-");
+                        countCards++;
+                    } else if(isDamaged && isDown){
+                        System.out.println("~**~");
+                        countCards++;
+                    } else if(isDamaged && isTorn){
+                        System.out.println("-~" + card + suitSymbol + "~-");
+                        countCards++;
+                    } else if(isDamaged){
+                        System.out.println("~" + card + suitSymbol + "~");
+                        countCards++;
+                    } else if(isTorn){
+                        System.out.println("-" + card +suitSymbol + "-");
+                        countCards++;
+                    } else if(isDown){
+                        System.out.println("[**]");
+                        countCards++;
+                    } else{
+                        System.out.println("[" + card + suitSymbol + "]");
+                        countCards++;
+                    }
+                }
+
+            }
+
+        }
+
+        System.out.println(countCards);
+    }
 }
